c15379c5938732acba7a2f07c2c3b128f0a3a24c
Steve Powell <steve.powell@suntrust.com>
2018-11-27 12:19:36 -0500

    Use my Imagemagick package

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,5 +1,5 @@
 pkg_name=habitatize
 pkg_origin=powell61
 pkg_scaffolding="core/scaffolding-ruby"
-pkg_version="0.1.0"
-pkg_deps=( core/imagemagick )
+pkg_version="0.2.0"
+pkg_deps=( powell61/imagemagick )
